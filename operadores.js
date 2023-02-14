ab78ba58351f5fa1e7494091d4fa59d773bc7ac2
Daniel Fabiani <daniaaron2@gmail.com>
2023-02-14 17:06:34 -0300

    add homework M3

---
+++ operadores.js
@@ -0,0 +1,24 @@
+//operadores y procedencia
+// + - * / 
+
+console.log(3+5);
+
+//Modulo
+console.log(95%4);
+
+//Procedencia
+
+console.log(3 + 5 * 2 -8);
+// = 5
+
+console.log((3 + 5) * 2 -8);
+// = 8
+
+//Adicion y strings
+
+console.log('Hola' + 'Daniel');
+
+var nombre = 'Daniel';
+var saludo = 'hola ' + nombre;
+
+console.log(saludo);
